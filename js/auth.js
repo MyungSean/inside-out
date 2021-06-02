8b8060a4d5984e08a45b1e6e74d1714323368bea
MyungSean <seansarang414@gmail.com>
2021-06-03 00:21:47 +0900

    signup and login

--- js/auth.js
+++ js/auth.js
@@ -3,7 +3,7 @@ auth.onAuthStateChanged(user => {
     if (user) {
         console.log('user logged in: ', user);
     } else {
-        console.log('userlogged out');
+        console.log('user logged out');
     }
     setupHeader(user);
 })
@@ -30,32 +30,141 @@ $('.logoutBtn').click(function(e) {
 
 
 // 회원가입
+function signupFormCheck() {
+    const name = $('#signup_name').val();
+    const email = $('#signup_email').val();
+    const password = $('#signup_pw').val();
+    const password_check = $('#signup_pw_check').val();
+
+    if ( name.length < 2 ) {
+        $('#signup_name').siblings('span').html('닉네임을 두 글자 이상 입력하세요.');
+        $('#signup_name').focus();
+        return false;
+    }
+    database.ref('users').orderByChild('name').equalTo(name).once('value').then(function(snapshot) {
+        if ( snapshot.val() !== null ) {
+            $('#signup_name').siblings('span').html('이미 존재하는 닉네임입니다.');
+            $('#signup_name').focus();
+            return false;
+        }
+    })
+
+    if ( password !== password_check ) {
+        $('#signup_pw_check').siblings('span').html('비밀번호가 일치하지 않습니다.');
+        $('#signup_pw_check').focus();
+        return false;
+    }
+
+    return true;
+}
+
+function password_check() {
+    var password = $('#signup_pw').val();
+    var password_check = $('#signup_pw_check').val();
+    if ( password == password_check ) {
+        $('#signup_pw_check').siblings('span').html('비밀번호가 일치합니다.');
+        $('#signup_pw_check').siblings('span').addClass('correct');
+    } else {
+        $('#signup_pw_check').siblings('span').html('비밀번호가 일치하지 않습니다.');
+        $('#signup_pw_check').siblings('span').removeClass('correct');
+    }
+}
+
+function signupErrorHandling(error) {
+    console.log('error code : ' +  error.code);
+    console.log('error message : ' +  error.message);
+    switch (error.code) {
+        case "auth/invalid-email":
+          errorMessage = "이메일 형식이 올바르지 않습니다.";
+          $('#signup_email').siblings('span').html(errorMessage);
+          $('#signup_email').focus();
+          break;
+        case "auth/email-already-in-use":
+          errorMessage = "이미 사용중인 이메일 주소입니다.";
+          $('#signup_email').siblings('span').html(errorMessage);
+          $('#signup_email').focus();
+          break;
+          case "auth/weak-password":
+              errorMessage = "비밀번호를 6자리 이상 입력해주세요.";
+              $('#signup_pw').siblings('span').html(errorMessage);
+              $('#signup_pw').focus();
+          break;
+        default:
+          alert('알 수 없는 오류가 발생했습니다.\n'+error.message);
+    }  
+}
+
 $('#signup_form #signup_submit').click(function(e) {
     e.preventDefault();
 
+    // 에러 메시지 초기화
+    $('#signup_form span').html("");
+
     const email = $('#signup_email').val();
     const password = $('#signup_pw').val();
+    const password_check = $('#signup_pw_check').val();
+
+    const name = $('#signup_name').val();
+
+    if ( !signupFormCheck() ) {
+        return;
+    }
 
     // 회원가입
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-        // console.log(cred.user);
+    auth.createUserWithEmailAndPassword(email, password).then(result => {
+        console.log(result.user);
+        var uid = result.user.uid;
+
+        database.ref('users/'+uid).set({
+            name: name,
+            photoUrl: false
+        })
+        .then(function() {
+            window.location.href = "/";
+        })
     })
+    .catch((error) => {
+        signupErrorHandling(error);
+    });        
 })
 
 // 소셜 로그인
-// 구글 계정으로 로그인
-$('#loginWithGoogle').click(function() {
-    var provider = new firebase.auth.GoogleAuthProvider();
+$('.social_login_btn').click(function() {
+    var loginWith = $(this).attr('name');
+
+    if ( loginWith == "google" ) {
+        var provider = new firebase.auth.GoogleAuthProvider();
+    } else if ( loginWith == "facebook" ) {
+        var provider = new firebase.auth.FacebookAuthProvider();
+    } else {
+        console.log('소셜 로그인 과정에서 오류가 발생했습니다.');
+        return;
+    }
+
     auth.signInWithPopup(provider).then((result) => {
+        console.log(result);
+        var user = result.user;
+        var uid = user.uid;
+        var name = user.displayName;
+        var photoURL = user.photoURL;
+
+        database.ref('users/'+uid).set({
+            name: name,
+            photoURL: photoURL
+        })
+
         /** @type {firebase.auth.OAuthCredential} */
         var credential = result.credential;
     
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
+    
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var accessToken = credential.accessToken;
         // ...
-      }).catch((error) => {
+    })
+    .catch((error) => {
+        console.log('error code : ' +  error.code);
+        console.log('error message : ' +  error.message);
+
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
@@ -64,53 +173,5 @@ $('#loginWithGoogle').click(function() {
         // The firebase.auth.AuthCredential type that was used.
         var credential = error.credential;
         // ...
-    });
-})
-
-// 페이스북 계정으로 로그인
-$('#loginWithFacebook').click(function() {
-    console.log('login btn clicked');
-    // var provider = new firebase.auth.FacebookAuthProvider();
-    // auth.signInWithPopup(provider).then((result) => {
-    //     console.log(result);
-    //     /** @type {firebase.auth.OAuthCredential} */
-    //     var credential = result.credential;
-    
-    //     // The signed-in user info.
-    //     var user = result.user;
-    
-    //     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    //     var accessToken = credential.accessToken;
-    //     // ...
-    // })
-    // .catch((error) => {
-    //     // Handle Errors here.
-    //     var errorCode = error.code;
-    //     var errorMessage = error.message;
-    //     // The email of the user's account used.
-    //     var email = error.email;
-    //     // The firebase.auth.AuthCredential type that was used.
-    //     var credential = error.credential;
-    //     // ...
-    // });  
-    var provider = new firebase.auth.FacebookAuthProvider();
-
-    auth.signInWithPopup(provider).then(function(result){
-      console.log(result);
-      console.log("success facebook account linked");
-  
-      var userName = result.user.displayName;
-      var userEmail = result.user.email
-  
-      database.ref('Users/' + result.user.uid + '/info').set({
-        Name: userName,
-        Email: userEmail
-      })
-  
-    }).catch(function(err){
-      console.log('error code : ' +  err.code);
-      console.log('error message : ' +  err.message);
-      // console.log(err);
-      console.log("fail to do");
-    })
+    });  
 })
